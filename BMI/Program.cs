6ad279c14252398b30f3a9dd5ea2618193ed927b
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-05-12 16:09:04 +0700

    Update Program

---
+++ BMI/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double weight, Height;
+// can nang nhan don vi KG
+Console.Write("Nhap can Nang(kg): ");
+weight = double.Parse(Console.ReadLine());
+// chieu cao su dung don vi met
+Console.Write("Nhap chieu cao(m): ");
+Height = double.Parse(Console.ReadLine());
+
+double bmi = weight / Math.Pow(Height,2);
+
+Console.Write("BMI " + bmi);
+
+if(bmi < 18.5)
+    Console.WriteLine("\nUnderWeight");
+else if(bmi < 25)
+    Console.WriteLine("\nNormal");
+else if(bmi <30)
+    Console.WriteLine("\nOverWeight");
+else if(bmi >= 30 )
+    Console.WriteLine("\nObese");
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 06_Caculator-BMI
+Tính chỉ số Sức khỏe của một người(BMI)
+    Công Thức: BIM = Weight / (Height * 2)
